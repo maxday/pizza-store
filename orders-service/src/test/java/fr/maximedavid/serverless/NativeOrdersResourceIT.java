62e9663e760b643f0ff8c57d2a5ab39bd93181e3
Maxime DAVID <contact@maximedavid.fr>
2020-11-12 11:10:26 -0500

    orders-service

--- orders-service/src/test/java/fr/maximedavid/serverless/NativeOrdersResourceIT.java
+++
@@ -1,9 +0,0 @@
-package fr.maximedavid.serverless;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeOrdersResourceIT extends OrdersResourceTest {
-
-    // Execute the same tests but in native mode.
-}
