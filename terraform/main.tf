0c90068bae8cdffe621fbbec5764d7468d72290b
Maxime DAVID <contact@maximedavid.fr>
2020-11-12 01:55:44 -0500

    client-frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -291,6 +291,22 @@ resource "google_cloud_run_domain_mapping" "pizza_maximedavid" {
   }
 }
 
+resource "google_cloud_run_domain_mapping" "orders_maximedavid" {
+  location = "us-central1"
+  name     = "orders.maximedavid.fr"
+
+  metadata {
+    namespace = var.project_id
+    annotations = {
+      "run.googleapis.com/launch-stage" : "BETA"
+    }
+  }
+
+  spec {
+    route_name = google_cloud_run_service.managerfrontend.name
+  }
+}
+
 output "client_front_end_url" {
   value = google_cloud_run_service.clientfrontend.status[0].url
 }
