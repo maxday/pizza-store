5439e754fac5562fbf187d7936c35d21a870a903
Maxime David <maxime.david@bnc.ca>
2020-11-17 14:10:06 -0500

    tests

--- client-orders-api/src/test/java/SimpleTest.java
+++ client-orders-api/src/test/java/SimpleTest.java
@@ -8,12 +8,18 @@ import com.google.cloud.pubsub.v1.TopicAdminSettings;
 import com.google.pubsub.v1.TopicName;
 import fr.maximedavid.serverless.GCPConfiguration;
 import fr.maximedavid.serverless.PizzaOrder;
+import fr.maximedavid.serverless.extension.ext.gcp.token.machine.TokenMachine;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.smallrye.mutiny.Uni;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -27,6 +33,16 @@ public class SimpleTest {
 
     @Inject
     GCPConfiguration configuration;
+
+    @Inject
+    TokenMachine tokenMachine;
+
+    @BeforeAll
+    public static void setup() {
+        TokenMachine mock = Mockito.mock(TokenMachine.class);
+        Mockito.when(mock.getAccessToken(null)).thenReturn(Uni.createFrom().item("fakeToken"));
+        QuarkusMock.installMockForType(mock, TokenMachine.class);
+    }
 
     @Test
     public void testSimple() {
